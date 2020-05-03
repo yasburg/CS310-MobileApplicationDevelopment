14a855785d5a71ac8c5c6a59ea2d978b9dbfe05b
yasburg <34233254+yasburg@users.noreply.github.com>
2020-05-03 23:27:26 +0300

    Week11.2

---
+++ Week11.2CS310MenuExample/app/src/main/java/edu/sabanciuniv/cs310menuexample/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.sabanciuniv.cs310menuexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Main Activity");
+    }
+
+}
