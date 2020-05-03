c5cf16164632981a45dd3240a9f6ed5997186e25
yasburg <34233254+yasburg@users.noreply.github.com>
2020-05-03 23:34:15 +0300

    Week11.3

---
+++ Week11.4startactivityandresult/app/src/main/java/edu/sabanciuniv/myapplication/Department.java
@@ -0,0 +1,66 @@
+package edu.sabanciuniv.myapplication;
+
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department implements Serializable {
+
+    private int id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        return name;
+
+
+    }
+
+    public static List<Department> getAllDepartments(){
+
+        List<Department> depts = new ArrayList<>();
+
+        Department dept1 = new Department(1,"IT");
+        Department dept2 = new Department(2,"SALES");
+        Department dept3 = new Department(3,"ACCOUNTING");
+        Department dept4 = new Department(4,"OPERATIONS");
+
+        depts.add(dept1);
+        depts.add(dept2);
+        depts.add(dept3);
+        depts.add(dept4);
+
+
+
+        return depts;
+    }
+
+}
